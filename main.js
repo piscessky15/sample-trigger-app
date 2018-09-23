6391238be52676c48af3e6a25735d3656e4cfd48
piscessky15 <pisces_sky15@yahoo.com>
2018-09-23 08:06:13 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,9 +5,14 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var request = require('request');
-var FCM = require('fcm-node');
-var serverKey = 'YAAAADF7mSTM:APA91bGNG6kOH6d3xb1ytRrCHXMfM-4rLHmFS2AfQ3oSiXAiXJtPNmSnE8sqWjArxR5LHIdIH-yi14cW6nnZealZHe5Q1kW2b7RtaFR0BWaahVWzzuRwNAL_Qtupb_UeUAPhbkYkLtuE'; //put your server key here
-var fcm = new FCM(serverKey);
+var admin = require("firebase-admin");
+
+var serviceAccount = require("firebase.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://depot-7ca22.firebaseio.com"
+});
 
 // Initialize express application that will handle trigger requests from the M2X API
 var app = express();
@@ -23,125 +28,125 @@ app.use(bodyParser.json());
 // Define HTTP endpoint that will receive trigger requests from AT&T M2X API
 app.post("/m2x-trigger", function (req, res) {
 
-  console.log("M2X POST request received");
+    console.log("M2X POST request received");
 
-  // Parse Custom Data sent with M2X Trigger Notification payload
-  var customData = JSON.parse(req.body.custom_data);
+    // Parse Custom Data sent with M2X Trigger Notification payload
+    var customData = JSON.parse(req.body.custom_data);
 
-  // Extract the delivery method from the Custom Metadata
-  var deliveryMethod = customData.delivery_method;
+    // Extract the delivery method from the Custom Metadata
+    var deliveryMethod = customData.delivery_method;
 
-  // Extract the recipient from the Custom Data
-  var recipient = customData.recipient;
+    // Extract the recipient from the Custom Data
+    var recipient = customData.recipient;
 
-  // Craft the message to be delivered via SMS or Email.
-  //
-  // In order to include more information in the message we pass the
-  // current value for each stream that triggered this event.
-  //
-  // If the trigger is configured to notify on reset, a reset notification 
-  // message will be delivered when the trigger has been reset.
-  //
-  // To see which data comes with the request, please check the
-  // AT&T M2X API documentation:
-  // http://m2x.att.com/developer/documentation/v2/triggers#Test-Trigger
-  if (req.body.event == "fired") {
-    var message = "Conditions met for M2X Trigger named \'" + req.body.trigger + "\'. VALUES: { ";
-  } else {
-    var message = "M2X Trigger named \'" + req.body.trigger + "\' has been reset. VALUES: { ";
-  }
-  var values = req.body.values;
-  var valuesLength = Object.keys(values).length;
-  var iterations = 1;
-  for (var stream in values) {
-    message = message + stream + ": " + values[stream].value;
-
-    if (iterations == valuesLength) {
-      message = message + " }";
-      break;
-    } else {
-      message = message + ", ";
-      iterations++;
-    }
-  }
-
-  console.log("Preparing to send message: " + message);
-  console.log("to: " + recipient);
-
-  if (deliveryMethod == "notification") {
-    var message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
-      "to": recipient,
-      data: {
-        message: message,
-        parcelId: "EMS1231513123",
-        etaHour: "3",
-        etaMinute: "25",
-        address: "No.1, Jalan BA 3, Taman K, Selangor"
-      }
-    };
-
-    fcm.send(message, function (err, response) {
-      if (err) {
-        console.log("Something has gone wrong!");
-      } else {
-        console.log("Successfully sent with response: ", response);
-      }
-    });
-
-  } else if (deliveryMethod == "hook") {
-    request.post({
-      headers: {
-        'content-type': 'application/x-www-form-urlencoded',
-        'Accepts': 'application/json'
-      },
-      url: 'https://webhook.site/400a74b0-74fa-4c53-8b84-f96c60e77668',
-      form: {
-        to: recipient,
-        message: message,
-        data: customData
-      }
-    }, function (error, response, body) {
-      if (!error && response.statusCode == 201) {
-        console.log('Message sent!');
-      } else {
-        var apiResult = JSON.parse(body);
-        console.log('Error was: ' + apiResult.message);
-      }
-    })
-
-  } else if (deliveryMethod == "email") {
-    // Extract the sender from the Custom Data.
+    // Craft the message to be delivered via SMS or Email.
     //
-    // Sender is only applicable to email delivery,
-    // while testing you can use the same email address
-    // for sender & recipient
-    var sender = customData.sender;
+    // In order to include more information in the message we pass the
+    // current value for each stream that triggered this event.
+    //
+    // If the trigger is configured to notify on reset, a reset notification 
+    // message will be delivered when the trigger has been reset.
+    //
+    // To see which data comes with the request, please check the
+    // AT&T M2X API documentation:
+    // http://m2x.att.com/developer/documentation/v2/triggers#Test-Trigger
+    if (req.body.event == "fired") {
+        var message = "Conditions met for M2X Trigger named \'" + req.body.trigger + "\'. VALUES: { ";
+    } else {
+        var message = "M2X Trigger named \'" + req.body.trigger + "\' has been reset. VALUES: { ";
+    }
+    var values = req.body.values;
+    var valuesLength = Object.keys(values).length;
+    var iterations = 1;
+    for (var stream in values) {
+        message = message + stream + ": " + values[stream].value;
 
-    // Define a subject for the email
-    var emailSubject = "Trigger notification";
+        if (iterations == valuesLength) {
+            message = message + " }";
+            break;
+        } else {
+            message = message + ", ";
+            iterations++;
+        }
+    }
 
-    // Initialize SendGrid object
-    var sendgrid = require("sendgrid")(
-      process.env.SENDGRID_USERNAME,
-      process.env.SENDGRID_PASSWORD
-    );
+    console.log("Preparing to send message: " + message);
+    console.log("to: " + recipient);
 
-    sendgrid.send({
-      from: sender,
-      to: recipient,
-      subject: emailSubject,
-      text: message
-    });
-  }
+    if (deliveryMethod == "notification") {
+        var message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
+            token: recipient,
+            data: {
+                message: message,
+                parcelId: "EMS1231513123",
+                etaHour: "3",
+                etaMinute: "25",
+                address: "No.1, Jalan BA 3, Taman K, Selangor"
+            }
+        };
+        admin.messaging().send(message)
+            .then((response) => {
+                // Response is a message ID string.
+                console.log('Successfully sent message:', response);
+            })
+            .catch((error) => {
+                console.log('Error sending message:', error);
+            });
 
-  res.send("OK");
+    } else if (deliveryMethod == "hook") {
+        request.post({
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded',
+                'Accepts': 'application/json'
+            },
+            url: 'https://webhook.site/400a74b0-74fa-4c53-8b84-f96c60e77668',
+            form: {
+                to: recipient,
+                message: message,
+                data: customData
+            }
+        }, function (error, response, body) {
+            if (!error && response.statusCode == 201) {
+                console.log('Message sent!');
+            } else {
+                var apiResult = JSON.parse(body);
+                console.log('Error was: ' + apiResult.message);
+            }
+        })
+
+    } else if (deliveryMethod == "email") {
+        // Extract the sender from the Custom Data.
+        //
+        // Sender is only applicable to email delivery,
+        // while testing you can use the same email address
+        // for sender & recipient
+        var sender = customData.sender;
+
+        // Define a subject for the email
+        var emailSubject = "Trigger notification";
+
+        // Initialize SendGrid object
+        var sendgrid = require("sendgrid")(
+            process.env.SENDGRID_USERNAME,
+            process.env.SENDGRID_PASSWORD
+        );
+
+        sendgrid.send({
+            from: sender,
+            to: recipient,
+            subject: emailSubject,
+            text: message
+        });
+    }
+
+    res.send("OK");
 });
 
 app.get("/status", function (req, res) {
-  res.send("Application is running!");
+    res.send("Application is running!");
 });
 
 
 app.listen(app.get("port"), function () {
-  console.log("Node app is running at localhost:" + app.get("port"));
+    console.log("Node app is running at localhost:" + app.get("port"));
 });
